a70a9098e4ed72fe815307582b3242f739419039
Jon <jxxwhite@gmail.com>
2014-08-02 23:28:29 +0100

    Keyboard shortcuts.

--- resource.h
+++ resource.h
@@ -9,7 +9,6 @@
 #define ID_BUTTON32772                  32772
 #define ID_TRIGGER                      32772
 #define ID_ZOOMIN                       32773
-#define ID_BUTTON32774                  32774
 #define ID_ZOOMOUT                      32774
 
 // Next default values for new objects
@@ -17,7 +16,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        310
-#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_COMMAND_VALUE         32781
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
